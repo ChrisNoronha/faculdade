eaedbc83c6c2d3662bab0da5153b8b1ed64f0e9e
Christian Picoli <124415337+ChrisNoronha@users.noreply.github.com>
2024-05-03 15:51:51 -0300

    Delete Programação Concorrente directory

--- Programação Concorrente/projetoHotel/Atividade2/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Hotel hotel = new Hotel(10);
-
-        // Criação e inicialização de threads para hóspedes, camareiras, recepcionistas
-    }
-}
